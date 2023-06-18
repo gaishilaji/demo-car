fb3a521fce60abc75121d5dfd3e76227bffaceab
JWJ <jwj233mail@163.com>
2023-06-18 18:23:04 +0800

    RoadPoint组件配置

--- assets/script/RoadPoint.ts
+++ assets/script/RoadPoint.ts
@@ -1,4 +1,4 @@
-import {_decorator, Component, Node, Vec3} from 'cc';
+import {_decorator, Component, Node, Vec3, Enum} from 'cc';
 
 const {ccclass, property} = _decorator;
 
@@ -11,47 +11,85 @@ enum RoadPointType {
   END,
   AI_START
 }
+Enum(RoadPointType)
 
 enum MoveType {
   LINE,
   CURVE
 }
+Enum(MoveType)
 
 @ccclass('RoadPoint')
-export class roadPoint extends Component {
+export class RoadPoint extends Component {
   @property({
-    type: RoadPointType
+    type: RoadPointType,
+    displayOrder: 1,
+    displayName: '道路点类型'
   })
   type = RoadPointType.NORMAL
 
   @property({
-    type: MoveType
+    type: MoveType,
+    displayOrder: 2,
+    displayName: '移动类型',
+    visible: function (this: RoadPoint) {
+      return this.type !== RoadPointType.END
+    }
   })
   moveType = MoveType.LINE;
 
   @property({
-    type: Node
+    type: Node,
+    displayOrder: 3,
+    displayName: '下个点',
+    visible: function (this: RoadPoint) {
+      return this.type !== RoadPointType.END
+    }
   })
   nextStation: Node = null;
 
-  @property
+  @property({
+    displayOrder: 4,
+    displayName: '顺时针',
+    visible: function (this: RoadPoint) {
+      return this.type !== RoadPointType.END && this.moveType === MoveType.CURVE
+    }
+  })
   clockwise = true;
 
   @property({
-    type: Vec3
+    visible: function (this: RoadPoint) {
+      return this.type === RoadPointType.GREETING || this.type === RoadPointType.GOODBYE
+    }
   })
   direction = new Vec3(1,0,0);
 
-  @property
+  @property({
+    visible: function (this: RoadPoint) {
+      return this.type === RoadPointType.AI_START
+    }
+  })
   interval = 3;
 
-  @property
+  @property({
+    visible: function (this: RoadPoint) {
+      return this.type === RoadPointType.AI_START
+    }
+  })
   delayTime = 0;
 
-  @property
+  @property({
+    visible: function (this: RoadPoint) {
+      return this.type === RoadPointType.AI_START
+    }
+  })
   speed = 0.05;
 
-  @property
+  @property({
+    visible: function (this: RoadPoint) {
+      return this.type === RoadPointType.AI_START
+    }
+  })
   car = '201';
 
 }
